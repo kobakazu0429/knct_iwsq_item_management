889ea41c37fb6cc1630efba4c7fd50ee3b15b209
kobakazu0429 <kobakazu10@gmail.com>
2022-10-26 02:33:51 +0900

    update utils

--- lib/item/utils.ts
+++ lib/item/utils.ts
@@ -1,5 +1,15 @@
+import { match } from "ts-pattern";
 import { customAlphabet } from "nanoid";
-import { getYear, lastDayOfMonth, isAfter, closestTo, set } from "date-fns";
+import {
+  parseISO,
+  getYear,
+  lastDayOfMonth,
+  isAfter,
+  closestTo,
+  set,
+} from "date-fns";
+import { format } from "date-fns-tz";
+import { type ItemSchema } from "./index";
 
 export const itemId = customAlphabet("abcdefhkmnpqrstwxyz", 9);
 
@@ -23,4 +33,28 @@ export const getNextExpiresDate = (currentDate: Date): Date => {
     currentDate,
     expiresDates.filter((date) => isAfter(date, currentDate))
   )!;
+};
+
+export const formatDate = (isoDate: string) => {
+  return format(parseISO(isoDate), "yyyy/MM/dd HH:mm:ss", {
+    timeZone: "Asia/Tokyo",
+  });
+};
+
+export const formatStatus = (status: ItemSchema["status"]): string => {
+  return match(status)
+    .with("verifying", () => "確認待ち")
+    .with("keeping", () => "保管中")
+    .with("removed", () => "撤去済み")
+    .with("removing", () => "撤去待ち")
+    .exhaustive();
+};
+
+export const formatChiefEmailVerified = (
+  verified: ItemSchema["chief_email_verified"]
+): string => {
+  return match(verified)
+    .with(true, () => "確認済み")
+    .with(false, () => "確認中")
+    .exhaustive();
 };
